2d8ff05c6abbb7fe21a69e5633055cc8c3dd9ed3
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-07-08 17:38:41 -0600

    initial commit

--- src/com/skilldistillery/blackjack/app/BlackJackApp.java
+++ src/com/skilldistillery/blackjack/app/BlackJackApp.java
@@ -3,8 +3,17 @@ package com.skilldistillery.blackjack.app;
 public class BlackJackApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BlackJackApp app = new BlackJackApp();
+		app.play();
 	}
+	public void play() {
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
